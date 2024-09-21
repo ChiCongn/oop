3d4cc8d4626e11342bfc881f4af7dd6b51c4f300
ChiCongn <chicongvn1205@gmail.com>
2024-09-21 21:41:17 +0700

    add HW1, HW2 and HW3

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyen Chi Cong \t 23021481 \t K68CS3 \t ChiCongn \t chicongvn1205@gmail.com");
+        for (int i = 9; i >= 1; i--){
+            System.out.println(i + " bottles of beer on the wall, 9 bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
